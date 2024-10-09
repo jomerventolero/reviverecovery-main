1199c93c916e152dcfbf682654ea083e6e842933
jomerventolero <hiroventolero@gmail.com>
2024-10-09 23:00:05 +0800

    Refactor Save Options - removed save-as-copy

--- app/src/views/private/components/save-options.vue
+++ app/src/views/private/components/save-options.vue
@@ -33,10 +33,6 @@ const { t } = useI18n();
 				<v-list-item-content>{{ t('save_and_create_new') }}</v-list-item-content>
 				<v-list-item-hint>{{ translateShortcut(['meta', 'shift', 's']) }}</v-list-item-hint>
 			</v-list-item>
-			<v-list-item v-if="!disabledOptions?.includes('save-as-copy')" clickable @click="$emit('save-as-copy')">
-				<v-list-item-icon><v-icon name="done_all" /></v-list-item-icon>
-				<v-list-item-content>{{ t('save_as_copy') }}</v-list-item-content>
-			</v-list-item>
 			<v-list-item v-if="!disabledOptions?.includes('discard-and-stay')" clickable @click="$emit('discard-and-stay')">
 				<v-list-item-icon><v-icon name="undo" /></v-list-item-icon>
 				<v-list-item-content>{{ t('discard_all_changes') }}</v-list-item-content>
